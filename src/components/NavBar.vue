43040440c2570f723ca11d87e6f46b541535fb40
ImmerAlex <aleximm67@gmail.com>
2024-12-05 14:28:23 +0100

    ajout des liens de la navbar depuis une liste

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,55 +1,37 @@
 <template>
   <v-card>
-    <v-app-bar
-        color="primary"
-        dark
-    >
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-
+    <v-app-bar color="primary" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>Dr Mad</v-toolbar-title>
     </v-app-bar>
 
-    <v-navigation-drawer
-        v-model="drawer"
-        absolute
-        height="fit-content"
-        temporary
-    >
-      <v-list
-          dense
-          nav
-      >
-        <v-list-item-group
-            v-model="group"
-            active-class="primary--text text--accent-4"
-        >
-          <v-list-item>
+    <v-navigation-drawer v-model="drawer" absolute height="fit-content" temporary>
+      <v-list dense nav>
+        <v-list-item-group active-class="primary--text text--accent-4">
+          <router-link v-for="link in links" :key="link.title" :to="link.to" tag="v-list-item" class="d-flex align-center">
             <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
+              <v-icon>{{ link.icon }}</v-icon>
             </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Account</v-list-item-title>
-          </v-list-item>
+            <v-list-item-title>{{ link.title }}</v-list-item-title>
+          </router-link>
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
   </v-card>
 </template>
 
-<script>export default {
+<script>
+export default {
   name: "NavBar",
   data: () => ({
     drawer: false,
-    group: null,
+    links: [
+      { title: "Home", icon: "mdi-home", to: { name: "home" } },
+      { title: "About", icon: "mdi-bank", to: { name: "about" } },
+    ]
   }),
 };
 </script>
-<style>
 
+<style>
 </style>
