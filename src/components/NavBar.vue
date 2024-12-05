8674537eca93959aea87aca924b999cf60a02505
ImmerAlex <aleximm67@gmail.com>
2024-12-05 14:03:02 +0100

    setup style de la navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,43 +1,55 @@
 <template>
-  <v-app-bar
-      app
-      color="primary"
-      dark
-  >
-    <div class="d-flex align-center">
-      <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-      />
-
-      <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-      />
-    </div>
-
-    <v-spacer></v-spacer>
-
-    <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+  <v-card>
+    <v-app-bar
+        color="primary"
+        dark
     >
-      <span class="mr-2">Latest Release</span>
-      <v-icon>mdi-open-in-new</v-icon>
-    </v-btn>
-  </v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Dr Mad</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        height="fit-content"
+        temporary
+    >
+      <v-list
+          dense
+          nav
+      >
+        <v-list-item-group
+            v-model="group"
+            active-class="primary--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </v-card>
 </template>
-<script>
-export default {
-  name: 'NavBar'
-}
+
+<script>export default {
+  name: "NavBar",
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+};
 </script>
+<style>
+
+</style>
