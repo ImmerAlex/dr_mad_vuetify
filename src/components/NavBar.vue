a9013c4921f571b55c60c1db6d953e10e9c67db6
Olivier Maraval <olivier.maraval@edu.univ-fcomte.fr>
2024-12-18 09:26:12 +0100

    fix(navbar): l'item surligné est uniquement celui de la route en cours
    le router-link vide à l'intérieur du v-list-item avait déjà une
    propriété to. C'est redondant et peut causer des problèmes de sélection.
    La v-list-item-group a été liée à une valeur pour gérer correctement la
    sélection active.
    
    Suppression du <router-link> redondant
    Ajout de la prop exact sur les v-list-item pour une correspondance
    exacte des routes
    Ajout d'une variable selectedItem dans le data()
    Liaison de la v-list-item-group avec v-model="selectedItem"
    Ajout d'une prop :value="index" sur chaque v-list-item
    Ajout d'un watcher sur la route pour réinitialiser la sélection lors du
    changement de pageE

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,27 +7,35 @@
 
     <v-navigation-drawer v-model="drawer" absolute height="100dvh" temporary>
       <v-list dense nav>
-        <v-list-item-group active-class="primary--text text--accent-4">
-
-          <template v-for="link in links">
-            <v-list-item v-if="link.to" :key="link.title" :to="link.to" class="d-flex align-center">
-              <router-link>
-            </router-link>
+        <v-list-item-group v-model="selectedItem" active-class="primary--text text--accent-4">
+          <template v-for="(link, index) in links">
+            <v-list-item 
+              v-if="link.to" 
+              :key="link.title" 
+              :to="link.to" 
+              :value="index"
+              class="d-flex align-center"
+              exact
+            >
               <v-list-item-icon>
                 <v-icon>{{ link.icon }}</v-icon>
               </v-list-item-icon>
               <v-list-item-title>{{ link.title }}</v-list-item-title>
             </v-list-item>
 
-            <v-list-item v-else :key="link.title" class="d-flex align-center" @click="handleAction(link.action)">
+            <v-list-item 
+              v-else 
+              :key="link.title" 
+              :value="index"
+              class="d-flex align-center" 
+              @click="handleAction(link.action)"
+            >
               <v-list-item-icon>
                 <v-icon>{{ link.icon }}</v-icon>
               </v-list-item-icon>
               <v-list-item-title>{{ link.title }}</v-list-item-title>
             </v-list-item>
-
           </template>
-
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
@@ -35,12 +43,13 @@
 </template>
 
 <script>
-import {mapActions, mapGetters} from "vuex";
+import { mapActions, mapGetters } from "vuex";
 
 export default {
   name: "NavBar",
   data: () => ({
     drawer: false,
+    selectedItem: null,
   }),
   computed: {
     ...mapGetters('user', ['isLogged']),
@@ -48,23 +57,16 @@ export default {
       const links = [];
 
       if (!this.isLogged) {
-
-        links.push({title: "Login", icon: "mdi-login", to: {'name': 'shoplogin'}});
-        links.push({title: "Register", icon: "mdi-account-plus", to: {'name': 'register'}});
-
+        links.push({title: "Login", icon: "mdi-login", to: {name: 'shoplogin'}});
+        links.push({title: "Register", icon: "mdi-account-plus", to: {name: 'register'}});
       } else {
-
-        links.push({title: "Home", icon: "mdi-home", to: {'name': 'home'}});
-
-        links.push({title: "Shop", icon: "mdi-account", to: {'name': 'shophome'}});
-        links.push({title: "Buy", icon: "mdi-account", to: {'name': 'shopbuy'}});
-        links.push({title: "Order", icon: "mdi-account", to: {'name': 'shoporders'}});
-
-        links.push({title: "Viruses", icon: "mdi-virus", to: {'name': 'shopitems'}});
-        links.push({title: "Bank", icon: "mdi-bank", to: {'name': 'bankaccount'}});
-
+        links.push({title: "Home", icon: "mdi-home", to: {name: 'home'}});
+        links.push({title: "Shop", icon: "mdi-account", to: {name: 'shophome'}});
+        links.push({title: "Buy", icon: "mdi-account", to: {name: 'shopbuy'}});
+        links.push({title: "Order", icon: "mdi-account", to: {name: 'shoporders'}});
+        links.push({title: "Viruses", icon: "mdi-virus", to: {name: 'shopitems'}});
+        links.push({title: "Bank", icon: "mdi-bank", to: {name: 'bankaccount'}});
         links.push({title: "Logout", icon: "mdi-logout", action: 'logout'});
-
       }
 
       return links;
@@ -74,6 +76,12 @@ export default {
     ...mapActions('user', ['logout']),
     handleAction(action) {
       this[action]();
+    }
+  },
+  watch: {
+    '$route'() {
+      // Réinitialiser la sélection lors du changement de route
+      this.selectedItem = null;
     }
   }
 };
